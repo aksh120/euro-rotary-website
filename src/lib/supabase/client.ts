07cfbc2362169f8cf81ddb6f81772a8f82bc2b13
Akshat Apoorv <akshatgamer120@gmail.com>
2025-12-24 13:56:46 +0530

    Update client.ts

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,7 +1,16 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-export const createClient = () =>
-  createBrowserClient(
+export const createClient = () => {
+  if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+    if (typeof window === 'undefined') {
+      return createBrowserClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://aksh.supabase.co',
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'aksh'
+      );
+    }
+  }
+  return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   );
+};
